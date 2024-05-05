940e25d6d407cc0f209e32473d35be1b14c1d60a
thetnaingkbtg <thet_naing.t@kbtg.tech>
2024-05-05 15:46:19 +0700

    feat: add router

--- services/app.js
+++ services/app.js
@@ -1,8 +1,11 @@
 import { loadData } from './menu.js'
 import store from './store.js'
+import router from './router.js'
 
 window.app = {}
 app.store = store
+app.router = router
 window.addEventListener('DOMContentLoaded', () => {
+    app.router.init()
     loadData()
 })
---
+++ services/router.js
@@ -0,0 +1,55 @@
+const router = {
+    init(){
+        document.querySelectorAll("a").forEach(anchor => {
+            anchor.addEventListener("click", (event)=>{
+                event.preventDefault();
+                const path = event.target.getAttribute("href")
+                router.go(path)
+            })
+        })
+
+        router.go(location.pathname)
+        
+        window.addEventListener('popstate',(event)=>{
+            console.log(event.state)
+            router.go(event.state.path, false)
+        })
+    },
+    go(path, addToHistory = true){
+        if(addToHistory){
+            window.history.pushState({path}, null, path)
+        }
+
+        let pageElement = null;
+        console.log(path)
+        switch(path){
+            case "/":
+                pageElement = document.createElement("h1")
+                pageElement.textContent = "Menu"
+                break;
+            case "/order":
+                pageElement = document.createElement("h1")
+                pageElement.textContent = "Order"
+                break;
+            default:
+                if (path.startsWith("/product-")) {                
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    pageElement.dataset.productId = path.substring(path.lastIndexOf("-")+1);
+                }
+                break;
+        }
+
+        if(pageElement){
+            const main = document.querySelector("main")
+            main.innerHTML = ""
+            main.appendChild(pageElement)
+        }
+
+        window.scrollX = 0  
+        window.scrollY = 0
+    }
+}
+
+
+export default router
