d850590a540914971845716f21d596dd983033e5
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2024-05-05 21:11:56 +0700

    feat: warpped store inside proxy

--- services/store.js
+++ services/store.js
@@ -3,4 +3,18 @@ const store = {
     cart: []
 }
 
-export default store
+const proxiedStore = new Proxy(store, {
+    set(target, property, value){
+        if(property === 'menu'){
+            const event = new Event("appmenuchange")
+            window.dispatchEvent(event)
+        }
+        if(property === 'cart'){
+            const event = new Event("appcartchange")
+            window.dispatchEvent(event)
+        }
+        return true;
+    }
+})
+
+export default proxiedStore
